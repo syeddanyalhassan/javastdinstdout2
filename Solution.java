c8c0e9e200e40414b1aac676a65005df897c876c
danyal <syeddanyalhassan@gmail.com>
2022-11-05 20:53:45 +0400

    new files added

--- Solution.java
+++ Solution.java
@@ -1,31 +1,46 @@
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
-
+import java.security.*;
 public class Solution {
+ public static void main(String[] args) {
 
-static int B,H;
-    static boolean flag = true;
-    static {
-        
-        Scanner sc = new Scanner(System.in);
-        B = sc.nextInt();
-        H = sc.nextInt();
-        if(B<=0 || H<=0){
-            System.out.println("java.lang.Exception: Breadth and height must be positive");
-            flag = false;
-        }
+  DoNotTerminate.forbidExit();
+
+  try {
+   Scanner in = new Scanner(System.in);
+   int n = in .nextInt();
+   in.close();
+   //String s=???; Complete this line below
+
+   String s = Integer.toString(n);
+   
+   if (n == Integer.parseInt(s)) {
+    System.out.println("Good job");
+   } else {
+    System.out.println("Wrong answer.");
+   }
+  } catch (DoNotTerminate.ExitTrappedException e) {
+   System.out.println("Unsuccessful Termination!!");
+  }
+ }
+}
+
+//The following class will prevent you from terminating the code using exit(0)!
+class DoNotTerminate {
+
+ public static class ExitTrappedException extends SecurityException {
+
+  private static final long serialVersionUID = 1;
+ }
+
+ public static void forbidExit() {
+  final SecurityManager securityManager = new SecurityManager() {
+   @Override
+   public void checkPermission(Permission permission) {
+    if (permission.getName().contains("exitVM")) {
+     throw new ExitTrappedException();
     }
-
-public static void main(String[] args){
-		if(flag){
-			int area=B*H;
-			System.out.print(area);
-		}
-		
-	}//end of main
-
-}//end of class
-
+   }
+  };
+  System.setSecurityManager(securityManager);
+ }
+}
