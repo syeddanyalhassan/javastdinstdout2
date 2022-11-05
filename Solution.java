feb8d43107654c5fc894bcb69addde61bebd9924
danyal <syeddanyalhassan@gmail.com>
2022-11-05 23:30:16 +0400

    new files added

--- Solution.java
+++ Solution.java
@@ -1,36 +1,18 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+import java.util.Scanner;
 
 public class Solution {
 
-
-
-    private static final Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
-        int N = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+        scan.close();
+        // Write your code here.
 
-        scanner.close();
-
-            if (N%2 != 0 )
-            
-            System.out.println("Weird");
-            
-
-                if (N%2 == 0 )
-                
-                    if(N>=2 && N<=5 || N>20)
-                    System.out.println("Not Weird");
-
-                    else if(N>=6 && N<=20)
-                    System.out.println("Weird");
-                
-
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
     }
 }
