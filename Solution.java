2dfd404bdc7b23921946a2613a334ca22d7eb630
danyal <syeddanyalhassan@gmail.com>
2022-11-05 22:23:04 +0400

    new files added

--- Solution.java
+++ Solution.java
@@ -1,26 +1,20 @@
 import java.util.*;
-import java.text.*;
 
 public class Solution {
-    
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double payment = scanner.nextDouble();
-        scanner.close();
-        NumberFormat n = NumberFormat.getCurrencyInstance(Locale.US);
-        String us = n.format(payment);
-        NumberFormat n1 = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
-        String india = n1.format(payment);
-        NumberFormat n2 = NumberFormat.getCurrencyInstance(Locale.CHINA);
-        String china = n2.format(payment);
-        NumberFormat n3 = NumberFormat.getCurrencyInstance(Locale.FRANCE);
-        String france = n3.format(payment);
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b= scan.nextInt();
+        int c=scan.nextInt();
+        // Complete this line
+        // Complete this line
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
         
-        // Write your code here.
-        
-        System.out.println("US: " + us);
-        System.out.println("India: " + india);
-        System.out.println("China: " + china);
-        System.out.println("France: " + france);
+        // Complete this line
+        // Complete this line
     }
 }
