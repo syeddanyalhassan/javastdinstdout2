e117ea91327359e5bb78fe6e67c1f0aeb7b6ed85
danyal <syeddanyalhassan@gmail.com>
2022-11-05 20:25:30 +0400

    new files added

--- Solution.java
+++ Solution.java
@@ -1,21 +1,49 @@
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.io.*;
 
-public class Solution {
 
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner sc=new Scanner(System.in);
-        int i=1;
-        while(sc.hasNext())
-        {
-            String newstr=sc.nextLine();
-            System.out.println(i+" "+newstr);
-            i++;
+
+class Solution{
+    
+    static String whoCanFitTheNumber(String numString)
+    {
+        String answer = "";
+        try{
+            long num = Long.parseLong(numString);
+            answer = numString + " can be fitted in:\n";
+            if((num<=Byte.MAX_VALUE) && (num>=Byte.MIN_VALUE)){
+                answer = answer.concat("* byte\n* short\n* int\n* long");
+            }else if((num <= Short.MAX_VALUE) && (num >= Short.MIN_VALUE)){
+                answer = answer.concat("* short\n* int\n* long");
+            }else if((num <= Integer.MAX_VALUE) && (num >= Integer.MIN_VALUE)){
+                    answer = answer.concat("* int\n* long");
+            }else{
+                    answer = answer.concat("* long");
+            }
+        }catch (NumberFormatException e){
+            answer = numString+" can't be fitted anywhere.";
         }
-        
+        return answer;
+    }
+    
+    public static void main(String []argh)
+    {
+
+
+ /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution.
+        */
+        Scanner scanner = new Scanner(System.in);
+        int numTestCases = scanner.nextInt() ;
+        scanner.nextLine();
+        for(int i=0; i<numTestCases;i++){
+            String numString = scanner.nextLine();
+            System.out.println(whoCanFitTheNumber(numString));
+        }
+       
+
+       
     }
 }
+
+
+
